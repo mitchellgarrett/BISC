0cece6745eb77b1de5a63e628302d042b162a88b
mitchellgarrett <45768600+mitchellgarrett@users.noreply.github.com>
2023-01-05 22:01:13 -0700

    Add load/store pseudo-instructions for labels

--- programs/test.asm
+++ programs/test.asm
@@ -1,5 +1,13 @@
-addi r0, r1, 5
-call function
-hlt
-function:
-ret
+section .text
+
+global main
+main:
+	la rt, var_dword
+	ld rv, rt[0]
+	ret
+
+section .data
+	var_dword: .dword 45634566534
+	var_word: .word 6535
+	var_byte: 255
+	var_string: .string "howdy world\n\0"
