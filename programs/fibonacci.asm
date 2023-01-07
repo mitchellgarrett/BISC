f798cdc707718dab03c007d02d78f3e8ead1bfd5
mitchellgarrett <45768600+mitchellgarrett@users.noreply.github.com>
2023-01-06 19:17:53 -0700

    Update fibonacci.asm to follow calling convention

--- programs/fibonacci.asm
+++ programs/fibonacci.asm
@@ -1,24 +1,35 @@
-; Calculates nth Fibonacci number
-; Returns Fibonacci number in rv
+; calculates nth Fibonacci number
+; returns Fibonacci number in rv
 
-push 5         ; get nth fibo number
+li r0, 5       ; get nth fibo number
 call fibonacci ; call fibonacci function
-addi sp, 4
 hlt            ; exit program
 
 fibonacci:
-	pop r0
-	dec r0         ; Subtract 1 from r0 as we already have first 2 numbers
+	subi sp, 16  ; allocate space of stack for stored registers
+	sw r0, sp[0] ; save registers modified by this function
+	sw r1, sp[4]
+	sw r2, sp[8]
+	sw r7, sp[12]
+
+	dec r0         ; subtract 1 from r0 as we already have first 2 numbers
 	li r7, 0
 	jle fibonacci_done, r0, r7 ; if n <= 0, return
-	li r1, 0       ; Initialize first fibo number
-	li r2, 1       ; Initialize second fibo number
-	li rv, 0       ; Initialize return value
+	li r1, 0       ; initialize first fibo number
+	li r2, 1       ; initialize second fibo number
+	li rv, 0       ; initialize return value
+
 fibonacci_loop:
-	add rv, r1, r2 ; Calculate next fibo number
-	mov r1, r2     ; Shift fibos down
+	add rv, r1, r2 ; calculate next fibo number
+	mov r1, r2     ; shift fibos down
 	mov r2, rv
-	dec r0         ; Decrement loop counter
-	jgt fibonacci_loop, r0, r7 ; Loop if not zero
+	dec r0         ; decrement loop counter
+	jgt fibonacci_loop, r0, r7 ; loop if not zero
+
 fibonacci_done:
-	ret            ; return from function, result in rv
+	ld r0, sp[0] ; restore modified registers to original values
+	ld r1, sp[0]
+	ld r2, sp[0]
+	ld r7, sp[0]
+	addi sp, 16 ; deallocate stack space
+	ret         ; return from function, result in rv
