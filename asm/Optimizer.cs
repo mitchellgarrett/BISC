1e6e21bc3a940b52f1f4c541d1612b15bd28f864
mitchellgarrett <45768600+mitchellgarrett@users.noreply.github.com>
2023-01-17 20:05:17 -0700

    Update Optimizer.cs

--- asm/Optimizer.cs
+++ asm/Optimizer.cs
@@ -5,8 +5,13 @@ namespace FTG.Studios.BISC.Assembler {
 		public static void Optimize(Program program) {
 			for (int i = 0; i < program.Instructions.Count; i++) {
 				Instruction inst = program.Instructions[i];
-				if (inst.Opcode == Opcode.LUI && inst.Parameters[1].Value == 0) {
-					program.Instructions.RemoveAt(i--);
+				if (i < program.Instructions.Count - 1) {
+					Instruction next = program.Instructions[i + 1];
+					// If current inst is LLI, next inst is LUI, both have same dest reg, and next inst loads 0
+					// then the LUI {reg}, 0 operation is redundant as LLI already zeros out the upper 16 bits
+					if (inst.Opcode == Opcode.LLI && next.Opcode == Opcode.LUI && inst.Parameters[0].Value == next.Parameters[0].Value && next.Parameters[1].Value == 0) {
+						program.Instructions.RemoveAt(i + 1);
+					}
 				}
 			}
 		}
